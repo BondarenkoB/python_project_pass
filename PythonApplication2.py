b57f491d1dbda9fd0f785ea845f1c5eefcfbff15
bohdan <bohdanstep23@gmail.com>
2023-04-09 10:53:49 +0300

    Well done

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -1,1 +1,14 @@
+#module
+import random
+import string
 
+ #creating pass gen
+
+# Combining the character types requested by the user
+characters = letters + numbers + special_chars
+
+# Generating the password
+password = "".join(random.choice(characters) for i in range(password_length))
+
+# Outputting the password
+print("Your password is:", password)
